ef87b0dd573c8c2045bfe6eecd66f08f698d3f5f
lisadebona <annalisa.quiamco@gmail.com>
2018-12-31 23:56:41 +0800

    Updated homepage

--- header.php
+++ header.php
@@ -38,32 +38,29 @@
 	$social_link = get_social_links();
 ?>
 
-	<div class="top_options">
-		<div class="wrapper">
-			<div class="socialmedia topdiv">
-				<div class="inside">
-				<?php foreach($social_link as $soc) { ?>
-					<?php if($soc['link']) { ?><a href="<?php echo $soc['link']; ?>"><i class="<?php echo $soc['icon']; ?>"></i></a><?php } ?>
-				<?php } ?>
-				</div>
-			</div>
-			<div class="linksdiv topdiv">
-				<?php if($member_link) { ?><div><a target="_blank" href="<?php echo $member_link; ?>">Board Member Log-In</a></div><?php } ?>
-				<?php if($subscribe_link) { ?><div><a target="_blank" href="<?php echo $subscribe_link; ?>">Subscribe to Newsletter</a></div><?php } ?>
-				<?php if($contact_link) { ?><div><a href="<?php echo $contact_link; ?>">Contact Us</a></div><?php } ?>
-			</div>
-			<div class="buttonsdiv topdiv">
-				<?php if($apply_link) { ?><a class="apply_link" href="<?php echo $apply_link; ?>" target="_blank">APPLY</a><?php } ?>
-				<?php if($donate_link) { ?><a class="donate_link" href="<?php echo $donate_link; ?>" target="_blank">DONATE</a><?php } ?>
-			</div>
-		</div>
-	</div>
-
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acstarter' ); ?></a>
 
 	<header id="masthead" class="site-header clear" role="banner">
 		<div class="wrapper headerflex">
-			
+			<div class="top_options">
+				<div class="socialmedia topdiv">
+					<div class="inside">
+					<?php foreach($social_link as $soc) { ?>
+						<?php if($soc['link']) { ?><a href="<?php echo $soc['link']; ?>"><i class="<?php echo $soc['icon']; ?>"></i></a><?php } ?>
+					<?php } ?>
+					</div>
+				</div>
+				<div class="linksdiv topdiv">
+					<?php if($member_link) { ?><div><a target="_blank" href="<?php echo $member_link; ?>">Board Member Log-In</a></div><?php } ?>
+					<?php if($subscribe_link) { ?><div><a target="_blank" href="<?php echo $subscribe_link; ?>">Subscribe to Newsletter</a></div><?php } ?>
+					<?php if($contact_link) { ?><div><a href="<?php echo $contact_link; ?>">Contact Us</a></div><?php } ?>
+				</div>
+				<div class="buttonsdiv topdiv">
+					<?php if($apply_link) { ?><a class="apply_link" href="<?php echo $apply_link; ?>" target="_blank">APPLY</a><?php } ?>
+					<?php if($donate_link) { ?><a class="donate_link" href="<?php echo $donate_link; ?>" target="_blank">DONATE</a><?php } ?>
+				</div>
+			</div>
+
 			<?php if(is_home()) { ?>
 	            <h1 class="logo">
 		            <a href="<?php bloginfo('url'); ?>">
